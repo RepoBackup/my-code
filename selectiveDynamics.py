e3f7797f8f0f55b38567d95a8f8f6d91bdc5b340
simplogic86 <45081142+simplogic86@users.noreply.github.com>
2019-09-19 07:57:25 -0500

    Update selectiveDynamics.py

--- selectiveDynamics.py
+++ selectiveDynamics.py
@@ -25,23 +25,3 @@ with open(loc + '/' + filename, 'r') as f, open(loc + '/SD.vasp', 'w') as g:
         for line in sorted(lines, key=lambda line: line[axis]):
             SD = ' F F F' if .17 * l < line[axis] < .36 * l else ' T T T'
             g.write('%18.9f %18.9f %18.9f' % tuple(line) + SD + ' \n')
-            
-#    trigger = 0
-#    for line in f.read().splitlines():
-#        if trigger == 0:
-#            if re.match(coordType, line):
-#                trigger = 1
-#                g.write('SD\n')
-#            SD = ''
-#        else:
-#            if axis == 'x':
-#                ax = float(line.split()[0])
-#            elif axis == 'y':
-#                ax = float(line.split()[1])
-#            else:
-#                ax = float(line.split()[2])
-#            if .13 * l < ax < .35 * l: # or .61 * l < ax < .89 * l:
-#                SD = ' F F F'
-#            else:
-#                SD = ' T T T'
-#        g.write(line + SD + '\n')
